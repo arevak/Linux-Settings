f01111db3309e81b08e60d38ec2c1e3321858d15
Andrew Revak <andyr@huddler-inc.com>
2012-02-29 20:22:38 +0000

    Added vim plugin installer from git submodule

--- init.py
+++ init.py
@@ -1,5 +1,26 @@
 #! /usr/bin/env python
 import os;
+import shutil;
+
+def install_vim_plugin(plugindir, branch=False):
+	externals_dir = os.getcwd()+"/externals";
+	pluginpath = externals_dir+"/"+plugindir;
+	targetpath = os.environ["HOME"]+"/.vim/"
+
+	print "Updating plugin from git";
+	if not branch:
+		branch = 'master';
+	os.chdir(pluginpath);
+	os.system('git pull origin '+branch);
+
+	print "Installing vim plugin at " + pluginpath;
+	for item in os.listdir(pluginpath):
+		ignorelist = ['.git']
+		if item not in ignorelist:
+			path = pluginpath+"/"+item;
+			if os.path.isdir(path):
+				cmd = 'cp -R ' + path + ' ' + targetpath;
+				os.system(cmd);
 
 def create_link(targetname, srcdir):
 	lntarget = os.environ["HOME"]+"/"+targetname;
@@ -13,6 +34,9 @@ create_link('.screenrc', 'screen');
 create_link('.vimrc', 'vim');
 create_link('.vim', 'vim');
 create_link('.ackrc', 'ack');
+
+# Update and install vim plugins in externals
+install_vim_plugin('vim-session');
 
 # For bash take the existing .bashrc rename it and relink it as .bashrc.local then link in this bashrc
 bashrc = os.environ["HOME"]+"/.bashrc";
